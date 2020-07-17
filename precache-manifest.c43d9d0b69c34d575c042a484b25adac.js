8a1d0956f5c868f1579a5570a4f5655cc045707e
Christopher Violante <c.violante211@gmail.com>
2020-07-17 14:54:33 -0600

    Updates

---
+++ precache-manifest.c43d9d0b69c34d575c042a484b25adac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f334a028d53f8be44631958beb04df59",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "375470b37d2032576031",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375470b37d2032576031",
+    "url": "/project-4-assessment/static/js/main.93966bbf.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
